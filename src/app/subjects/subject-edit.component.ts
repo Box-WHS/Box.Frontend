22df13c5dc1e0fbe3bb90dc471ab8c571f4952e9
Christian Klemm <chris579@web.de>
2017-11-24 11:44:09 +0100

    fck dis shit v2

--- src/app/subjects/subject-edit.component.ts
+++ src/app/subjects/subject-edit.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  templateUrl: './subject-edit.component.html'
+  templateUrl: './subject-edit.component.html',
+  styleUrls: ['./subject-edit.component.scss']
 })
 export class SubjectEditComponent {
   nameEditMode = false;
