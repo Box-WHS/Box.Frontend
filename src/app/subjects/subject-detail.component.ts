acbfb89007c5e88520f1d2ca1b24bc0b96c47f82
lniehaves <32952203+lniehaves@users.noreply.github.com>
2017-12-08 08:31:17 +0100

    small changes

--- src/app/subjects/subject-detail.component.ts
+++ src/app/subjects/subject-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Subject } from './subject';
 import { AppComponent } from '../app.component';
+import {Router} from "@angular/router";
 
 @Component({
   templateUrl: './subject-detail.component.html',
@@ -9,7 +10,11 @@ import { AppComponent } from '../app.component';
 export class SubjectDetailComponent {
   subject: Subject = { id: 0, name: 'Test', learnProgress: 20 };
 
-  constructor() {
+  constructor(private router: Router) {
     AppComponent.pageTitle = `Fach ${this.subject.name}`;
+  }
+
+  redirectToLearn() {
+    this.router.navigate(['learn']);
   }
 }
